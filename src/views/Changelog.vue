a93d60e3148c0b3a9c80ce81cb63e98219f8f78d
xfdxfdxfd <xdffxd@gmail.com>
2023-11-21 17:08:46 +0800

    add the changelog for the updates in 21/11/2023

--- src/views/Changelog.vue
+++ src/views/Changelog.vue
@@ -6,6 +6,10 @@
                 &nbsp;
                 <h2 style="border-bottom: 3px solid rgb(172, 172, 172);"></h2>
                 &nbsp;
+                <h3 class="box1h3text" style="text-align: left;font-size: 1.5rem;margin:10px">21-11-2023</h3>
+                <h3 class="box1h3text" style="text-align: left;color:#c2bfbf;margin:10px">{{ $t(`Changelog21-11-2023`) }}
+                </h3>
+                &nbsp;
                 <h3 class="box1h3text" style="text-align: left;font-size: 1.5rem;margin:10px">19-11-2023</h3>
                 <h3 class="box1h3text" style="text-align: left;color:#c2bfbf;margin:10px">{{ $t(`Changelog19-11-2023`) }}
                 </h3>
